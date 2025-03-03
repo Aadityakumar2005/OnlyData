468bd76b1521b97fcaf8c2928604038d8d3380cb
Aaditya Kumar <aadityakushwaha2005@gmail.com>
2025-03-04 01:45:34 +0530

    Update SignedOutPage.jsx

--- src/components/SignedOutPage.jsx
+++ src/components/SignedOutPage.jsx
@@ -12,13 +12,6 @@ export default function SignedOutPage() {
         <p className="text-gray-600 mb-6">
           Please sign in to access the more content.
         </p>
-        <button
-          className="px-4 py-2 bg-blue-500 text-white font-medium text-lg rounded-lg shadow hover:bg-blue-600 transition duration-200"
-        >
-          <SignedOut>
-            <SignInButton/>
-          </SignedOut>
-        </button>
       </div>
     </div>
   )
